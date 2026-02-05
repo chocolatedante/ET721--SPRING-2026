31567bbca19c875cb0b05014689734f944c95816
Dante Vanderpool <vanderpoolikes@gmail.com>
2026-02-05 18:49:31 +0000

    functions

---
+++ lab5/lab5_function_vanderpool.py
@@ -0,0 +1,34 @@
+"""
+Dante Vanderpool 
+Feb 5 2026
+Lab 5, function
+"""
+import math 
+#example 1
+def area_rectangle(width, length):
+    return width*length
+
+#void fucntion doesnt return a value
+def print_area_result(width, length, area):
+    print(f'The area of rectanlge of {width} and {length} is {area}')
+
+#example 2: calculate the distance of two points
+#distance = square_root of ((x2-x1)^2 + (y2-y1)^2)
+#function 1) collect a number between 1 and 10
+def collectnum():
+    num=int(input("Enter a number: "))
+    #use a loop to recollect the number if the number is not between 1 and 10 
+    while(num<1 or num>10):
+        num=int(input("Invlalid. Enter a number between 1 and 10: "))
+    return num
+
+#function that calcluates and returns the distance
+#distance = square_root of ((x2-x1)^2 + (y2-y1)^2)
+def calculate_distance(x1,x2,y1,y2):
+    return math.sqrt(math.pow((x2-x1),2)) + math.pow((y2-y1),2)
+
+#function to print the result
+def print_distance(x1,x2,y1,y2, distance):
+    print(f"The distance of points ({x1},{y1}) and ({x2},{y2} is {round(distance,2)})")
+
+    #EXERCISE
---
+++ lab5/lab5_vanderpool.py
@@ -0,0 +1,27 @@
+"""
+Dante Vanderpool 
+Feb 5 2026
+Lab 5, function
+"""
+import math
+from lab5_function_vanderpool import *
+
+print('\n---Example1: user-defined function---')
+w = 8
+length = 3
+a =area_rectangle(w,length)
+print_area_result(w,length, a)
+
+print("\n---Example 2: calculate the distance of two points")
+x1=collectnum('x1')
+x2=collectnum('x2')
+y1=collectnum('y1')
+y2=collectnum('y2')
+
+#testing
+#print(f"({x1},{y1})({x2},{y2})")
+
+distance = calculate_distance(x1,x2,y1,y2)
+print_distance(x1,x2,y1,y2,distance)
+
+print('\nEXERCISE')
