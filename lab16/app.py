abdf468fd498037a034b780f41422c7e47c41f0c
Dante Vanderpool <vanderpoolikes@gmail.com>
2026-04-16 19:37:15 +0000

    lab 16

--- lab16/app.py
+++ lab16/app.py
@@ -3,48 +3,49 @@ import mysql.connector
 
 app = Flask(__name__)
 
-#---------------
-#database config
-#---------------
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='flaskhost'
-app.config['MYSQL_PASSWORD']='password123'
-app.config['MYSQL_DB']='todo_db'
+# ---------------
+# database config
+# ---------------
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'flaskhost'
+app.config['MYSQL_PASSWORD'] = 'PassWord#123'
+app.config['MYSQL_DB'] = 'todo_db'
 
 db = mysql.connector.connect(
-    host = app.config['MYSQL_HOST'],
-    user = app.config['MYSQL_USER'],
-    password = app.config['MYSQL_PASSWORD'],
-    database = app.config['MYSQL_DB']
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    database=app.config['MYSQL_DB']
 )
 
-#--------------------
-#Home route
-#--------------------
+# --------------------
+# Home route
+# --------------------
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#--------
-#get all task
-#--------
-@app.route('/get_tasks', methods = ['GET'])
+# --------
+# get all tasks
+# --------
+@app.route('/get_tasks', methods=['GET'])
 def get_tasks():
     try:
-        with db.cursor(dictionary = True) as cursor:
-            cursor.execute("SELECT * FROM tasks")
-            tasks = cursor.fetchall()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM tasks")
+        tasks = cursor.fetchall()
+        cursor.close()
         return jsonify(tasks)
     except mysql.connector.Error as e:
-        return jsonify({"error : str(e)"}), 500
+        return jsonify({"error": str(e)}), 500
 
-#---------
-#add new task
-#--------
-@app.route('/add_task', methods = [POST])
+# ---------
+# add new task
+# --------
+@app.route('/add_task', methods=['POST'])
 def add_task():
     data = request.get_json()
-    task=data.get('task')
+    task = data.get('task')
 
     if task:
         cursor = db.cursor()
@@ -52,11 +53,26 @@ def add_task():
         db.commit()
         cursor.close()
         return jsonify({'status': 'success'})
-    
-    return jsonify({'status':'error'})
 
-#----------
-#run app
-#---------
+    return jsonify({'status': 'error'})
+
+# ------
+# delete a task
+# ------
+@app.route('/delete_task', methods=['POST'])
+def delete_task():
+    data = request.get_json()
+    task_id = data.get('id')
+
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM tasks WHERE id = %s", (task_id,))
+    db.commit()
+    cursor.close()
+
+    return jsonify({'status': 'deleted'})
+
+# ----------
+# run app
+# ---------
 if __name__ == '__main__':
     app.run(debug=True)
